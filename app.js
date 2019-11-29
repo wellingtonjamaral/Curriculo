f5494ef164aa70ecc3472db076d5beea1b6cc9dc
LeoCosta001 <leocosta.black@hotmail.com>
2019-11-29 09:43:22 -0300

    Adicionando Conteúdos=

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const path = require('path');
+const createError = require('http-errors')
 
-app.get('/',(req, res, next) =>{
-    res.send({
-        title: "teste",
-        version: "0.0.0"
-    });
-});
+//Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');  
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/', require('./routes/index'));
+app.use('/curriculo', require('./routes/curriculo'));
+app.use('/estudos', require('./routes/estudos'));
+
+// Resposta de Servidor Ligado
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`);
+});
+
+// 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
 });
